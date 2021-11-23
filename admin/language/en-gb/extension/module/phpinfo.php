f32ef4d06392024085733839f55f9af323ba3982
reailistuz <reailistuz@gmail.com>
2021-11-24 01:29:15 +0500

    11 - filter fix

---
+++ admin/language/en-gb/extension/module/phpinfo.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'PHP Info';
+
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified PHP Info!';   
+$_['text_refresh']        = 'Refresh';   
+
+// Tabs
+$_['tab_main']     = 'General';
+$_['tab_info']       = 'Info';
+
+// Info
+$_['info_about'] = 'PHP Info is an OCMOD extension that embeds inside your OpenCart Store the phpinfo() function. <br>This is helpful to know what server modules are enabled and to troubleshoot problems that you mite be experiencing.';
+$_['info_developer'] = 'OPnotes';
+$_['info_coffee'] = '<a href="https://www.buymeacoffee.com/opnotes" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/arial-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>';
+$_['info_support'] = '<a class="btn btn-primary" href="mailto:email@opnotes.eu"><i class="fa fa-life-ring"></i> Support</a>';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to access Store PHP Info!';
